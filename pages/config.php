44e0f7fdfdcaaec932588a5dc615740f070b4802
floresjr0 <marteflores07@gmail.com>
2026-08-23 07:22:34 +0800

    update config
    update email smtp

--- pages/config.php
+++ pages/config.php
@@ -10,12 +10,12 @@ const DB_USER = 'root';
 const DB_PASS = '';
 
 // Mail settings for PHPMailer (configure according to your SMTP provider)
-const MAIL_FROM_ADDRESS  = 'marteflores07@gmail.com';
+const MAIL_FROM_ADDRESS  = 'centrixomdrrmo@gmail.com'; //old email ko
 const MAIL_FROM_NAME     = 'MDRRMO San Ildefonso';
 const MAIL_SMTP_HOST     = 'smtp.gmail.com';
 const MAIL_SMTP_PORT     = 587;
-const MAIL_SMTP_USERNAME = 'marteflores07@gmail.com';
-const MAIL_SMTP_PASSWORD = 'sdayahycscuwagro';
+const MAIL_SMTP_USERNAME = 'centrixomdrrmo@gmail.com'; //old email ko
+const MAIL_SMTP_PASSWORD = 'mfbm souh dcwv bnnx'; //sdayahycscuwagro - old app just incase
 const MAIL_SMTP_SECURE   = 'tls';
 
 // Weather API (optional, used by cron_fetch_weather.php)
